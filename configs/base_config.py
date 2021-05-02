1a43197fb9944633e403e962de08742fb3cc03b5
Your Name <gsoykan20@login03.kuacc.ku.edu.tr>
2021-05-02 10:02:24 +0300

    add base config

---
+++ configs/base_config.py
@@ -0,0 +1,1 @@
+base_dir = "/kuacc/users/gsoykan20/projects/AF-GAN/"
--- utils/config_utils.py
+++ utils/config_utils.py
@@ -1,7 +1,7 @@
 import yaml
 from enum import Enum
 from collections import namedtuple
-
+from configs import base_config
 
 class Config(Enum):
     FACE_RECOGNITION = 1
@@ -9,7 +9,7 @@ class Config(Enum):
 
 def read_config(config: Config):
     if config == Config.FACE_RECOGNITION:
-        path = r'../configs/face_recognition_config.yaml'
+        path = base_dir + 'configs/face_recognition_config.yaml'
     with open(path) as file:
         configs = yaml.load(file, Loader=yaml.FullLoader)
     configs = namedtuple("Config", configs.keys())(*configs.values())
