ebe1a033d86f0d7ba656ea2f169aaddd62250443
gsoykan <gsoykan@sabanciuniv.edu>
2021-05-03 15:20:08 +0300

    update baseconfig for autodetection of environment

--- configs/base_config.py
+++ configs/base_config.py
@@ -1,17 +1,30 @@
 import enum
+import os
 
 
 # NOTE: Barış and Caghan you can also introduce your own local env
 # as B_LOCAL or C_LOCAL
 class Environment(enum.Enum):
     G_LOCAL = 1
-    CLUSTER = 2
-    B_LOCAL = 3
-    C_LOCAL = 4
+    B_LOCAL = 2
+    C_LOCAL = 3
+    G_CLUSTER = 4
+    B_CLUSTER = 5
+    C_CLUSTER = 5
 
-# TODO: write code for determining environment directly
+
+def determine_env() -> Environment:
+    cwd = os.getcwd()
+    if "/home/gsoykan20/Desktop/AF-GAN/" in cwd:
+        return Environment.G_LOCAL
+    elif "/kuacc/users/gsoykan20/" in cwd:
+        return Environment.G_CLUSTER
+    else:
+        raise NotImplementedError
+
+
 def determine_base_dir(env: Environment) -> str:
-    if env is Environment.CLUSTER:
+    if env is Environment.G_CLUSTER:
         return "/kuacc/users/gsoykan20/projects/AF-GAN/"
     elif env is Environment.G_LOCAL:
         return "/home/gsoykan20/Desktop/AF-GAN/"
@@ -19,5 +32,8 @@ def determine_base_dir(env: Environment) -> str:
         raise NotImplementedError
 
 
-environment = Environment.CLUSTER
+environment = determine_env()
 base_dir = determine_base_dir(environment)
+
+if __name__ == '__main__':
+    print(base_dir)
