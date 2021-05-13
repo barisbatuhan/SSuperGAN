e0b8eb59d569cf06462c84d9e724442a57647551
gsoykan <gsoykan@sabanciuniv.edu>
2021-05-13 13:31:46 +0300

    move fine forward to model

--- networks/ssupervae_contextual_attentional.py
+++ networks/ssupervae_contextual_attentional.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import torch
 import torch.nn as nn
 from torch import Tensor
@@ -59,6 +61,66 @@ class SSuperVAEContextualAttentional(BaseVAE):
         z = torch.distributions.Normal(mu, lg_std.exp()).rsample()
         x_recon = self.decode(z)
         return z, None, mu, x_recon, lg_std
+
+    # temporary function to forward pass for fine generation
+    def fine_generation_forward(self,
+                                x,
+                                y,
+                                mask,
+                                mu_x,
+                                mask_coordinates,
+                                interim_face_size):
+        # Preparing for Fine Generator
+        B, S, C, W, H = x.shape
+        mask = mask.view(B, 1, W, H)
+        x_stage_0 = ptu.zeros(B, C, H, W)
+        x_stage_1 = ptu.zeros_like(x_stage_0)
+        for i in range(len(x)):
+            last_panel = x[i, 2, :, :, :]
+            output_merged_last_panel = deepcopy(last_panel)
+
+            last_panel_face = y[i, :, :, :]
+            last_panel_output_face = mu_x[0, :, :, :]
+
+            mask_coordinates_n = mask_coordinates[i]
+
+            original_w = abs(mask_coordinates_n[0] - mask_coordinates_n[1])
+            original_h = abs(mask_coordinates_n[2] - mask_coordinates_n[3])
+
+            # inserting original face to last panel
+            modified = last_panel_face.view(1, *last_panel_face.size())
+            interpolated_last_panel_face_batch = torch.nn.functional.interpolate(modified,
+                                                                                 size=(original_w, original_h))
+            interpolated_last_panel_face = interpolated_last_panel_face_batch[0]
+            last_panel[:, mask_coordinates_n[0]: mask_coordinates_n[1],
+            mask_coordinates_n[2]: mask_coordinates_n[3]] = interpolated_last_panel_face
+            x_stage_0[i, :, :, :] = last_panel
+
+            # inserting output face to last panel
+            modified = last_panel_output_face.view(1, *last_panel_output_face.size())
+            interpolated_last_panel_face_batch = torch.nn.functional.interpolate(modified,
+                                                                                 size=(original_w, original_h))
+            interpolated_last_panel_face = interpolated_last_panel_face_batch[0]
+            output_merged_last_panel[:, mask_coordinates_n[0]: mask_coordinates_n[1],
+            mask_coordinates_n[2]: mask_coordinates_n[3]] = interpolated_last_panel_face
+            x_stage_1[i, :, :, :] = output_merged_last_panel
+
+        # TODO: x_stage_2 here is not in same normalized space i assume
+        # we should cross check with the implementation
+        # TODO: we need to normalize before visualizing
+        x_stage_2, offset_flow = self.fine_generator(x_stage_0, x_stage_1, mask)
+
+        fine_faces = ptu.zeros(B, C, interim_face_size, interim_face_size)
+        for i in range(len(x)):
+            x_stage_2_n = x_stage_2[i, :, :, :]
+            mask_coordinates_n = mask_coordinates[i]
+            fine_face = x_stage_2_n[:, mask_coordinates_n[0]: mask_coordinates_n[1],
+                        mask_coordinates_n[2]: mask_coordinates_n[3]]
+            interpolated_fine_face = torch.nn.functional.interpolate(fine_face.view(1, *fine_face.size()),
+                                                                     size=(interim_face_size, interim_face_size))
+            fine_faces[i, :, :, :] = interpolated_fine_face
+
+        return x_stage_2, offset_flow, fine_faces
 
     def encode(self, x):
         return self.encoder(x)
