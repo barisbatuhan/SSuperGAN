bee8f483e7a91de5802e9238df663013b43200ad
gsoykan <gsoykan@sabanciuniv.edu>
2021-05-18 10:15:12 +0300

    add elbo with L1

--- functional/losses/elbo.py
+++ functional/losses/elbo.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 import torch
 from functional.losses.kl_loss import kl_loss
-from functional.losses.reconstruction_loss import reconstruction_loss_distributional,l1_loss
+from functional.losses.reconstruction_loss import reconstruction_loss_distributional, l1_loss
 
 
 def elbo(z,
@@ -10,16 +10,16 @@ def elbo(z,
          mu_x,
          logstd_z,
          logstd_x=None,
-         kl_loss_weight=1, 
+         kl_loss_weight=1,
+         recon_loss_weight=1,
          l1_recon=False):
-    
-    kl_loss_value = kl_loss(z, mu_z, torch.exp(logstd_z)) 
+    kl_loss_value = kl_loss(z, mu_z, torch.exp(logstd_z))
     if not l1_recon:
-        reconstruction_loss = -1 * reconstruction_loss_distributional(mu_x, x, logstd_x)
+        reconstruction_loss = -recon_loss_weight * reconstruction_loss_distributional(mu_x, x, logstd_x)
     else:
-        reconstruction_loss = l1_loss(mu_x, x) 
+        reconstruction_loss = recon_loss_weight * l1_loss(mu_x, x)
     loss = reconstruction_loss + kl_loss_weight * kl_loss_value
-    
+
     return OrderedDict(loss=loss,
                        reconstruction_loss=reconstruction_loss,
                        kl_loss=kl_loss_value)
