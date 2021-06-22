06269d16ef4517bb206f453c9e2672babc53ac8c
gsoykan <gsoykan@sabanciuniv.edu>
2021-06-22 11:31:10 +0300

    fix parallel sampling and add psnr eval to val cycle

--- training/vae_trainer.py
+++ training/vae_trainer.py
@@ -1,10 +1,12 @@
 from tqdm import tqdm
 import torch
 
+from data.augment import get_PIL_image
 from training.base_trainer import BaseTrainer
 from utils.structs.metric_recorder import *
 from utils.logging_utils import *
 from utils.image_utils import *
+from functional.metrics.psnr import PSNR
 
 
 class VAETrainer(BaseTrainer):
@@ -19,6 +21,7 @@ class VAETrainer(BaseTrainer):
                  scheduler=None,
                  quiet: bool = False,
                  grad_clip=None,
+                 parallel=False,
                  best_loss_action=None,
                  save_dir=base_dir + 'playground/vae/',
                  checkpoint_every_epoch=False):
@@ -37,6 +40,7 @@ class VAETrainer(BaseTrainer):
         self.test_loader = test_loader
         self.optimizer = optimizer
         self.scheduler = scheduler
+        self.parallel = parallel
 
     def train_epochs(self, starting_epoch=None, losses={}):
         metric_recorder = MetricRecorder(experiment_name=self.model_name,
@@ -50,9 +54,9 @@ class VAETrainer(BaseTrainer):
             if starting_epoch is not None and starting_epoch >= epoch:
                 continue
             logging.info("epoch start: " + str(epoch))
-            train_loss = self.train_vae(epoch)
+            train_loss = self.train_model(epoch)
             if self.test_loader is not None:
-                test_loss = self.eval_loss(self.test_loader)
+                test_loss = self.eval_model(epoch)
             else:
                 test_loss = {"loss": 0, "kl_loss": 0, "reconstruction_loss": 0}
 
@@ -81,10 +85,11 @@ class VAETrainer(BaseTrainer):
         return train_losses, test_losses
 
     @torch.no_grad()
-    def eval_loss(self, data_loader):
+    def eval_model(self, epoch):
         self.model.eval()
+        psnrs, l1s, iter_cnt, recon_print, bs = 0, 0, 0, False, self.test_loader.batch_size
         total_losses = OrderedDict()
-        for batch in data_loader:
+        for batch in self.test_loader:
 
             if type(batch) == list and len(batch) == 2:
                 x, y = batch[0].cuda(), batch[1].cuda()
@@ -105,16 +110,47 @@ class VAETrainer(BaseTrainer):
             for k, v in out.items():
                 total_losses[k] = total_losses.get(k, 0) + v.item() * x.shape[0]
 
+            # TODO: THIS CAN BE MODULIZED
+            psnrs += PSNR.__call__(mu_x, y, fit_range=True)
+            l1s += torch.abs(y - mu_x).mean()
+            iter_cnt += 1
+
+            if recon_print == False:
+                recon_print = True
+                h, w = y.shape[2:]
+                wsize, hsize = 2, y.shape[0]
+                w = (w + 100) * wsize
+                h = (h + 100) * hsize
+                px = 1 / plt.rcParams['figure.dpi']
+                f, ax = plt.subplots(hsize, wsize)
+                f.set_size_inches(w * px, h * px)
+
+                for bs in range(y.shape[0]):
+                    ax[bs, 0].imshow(get_PIL_image(y[bs, :, :, :]))
+                    ax[bs, 0].axis('off')
+                    ax[bs, 1].imshow(get_PIL_image(mu_x[bs, :, :, :].clamp(-1, 1)))
+                    ax[bs, 1].axis('off')
+
+                ax[0, 0].title.set_text("Original")
+                ax[0, 1].title.set_text("Recon")
+
+                plt.savefig(self.save_dir + 'results/' + f'_epoch{epoch}_recons.png')
+
+        print("\n\n-- Epoch:", epoch,
+              " --> PSNR:", psnrs.item() / iter_cnt,
+              " & L1 Loss:", l1s.item() / iter_cnt,
+              "\n")
+
         desc = 'Test --> '
         for k in total_losses.keys():
-            total_losses[k] /= len(data_loader.dataset)
+            total_losses[k] /= len(self.test_loader.dataset)
             desc += f'{k} {total_losses[k]:.4f}'
         if not self.quiet:
             print(desc)
             logging.info(desc)
         return total_losses
 
-    def train_vae(self, epoch):
+    def train_model(self, epoch):
         self.model.train()
         if not self.quiet:
             pbar = tqdm(total=len(self.train_loader.dataset))
@@ -158,7 +194,12 @@ class VAETrainer(BaseTrainer):
                 pbar.update(x.shape[0])
 
         self.scheduler.step()
-        self.model.save_samples(10, self.save_dir + '/results/' + f'epoch{epoch}_samples.png')
+
+        if self.parallel:
+            self.model.module.save_samples(100, self.save_dir + 'results/' + f'_epoch{epoch}_samples.png')
+        else:
+            self.model.save_samples(100, self.save_dir + 'results/' + f'_epoch{epoch}_samples.png')
+
         if not self.quiet:
             pbar.close()
         return losses
